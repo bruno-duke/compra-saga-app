a40342bc9418ead642b46b0f16f1f45761c969af
Bruno Leite <brunoleite@Brunos-MacBook-Pro.local>
2022-10-18 19:14:58 -0300

    commit inicial

---
+++ compra-orquestrada/src/main/java/com/brunofsleite/PedidoService.java
@@ -0,0 +1,19 @@
+package com.brunofsleite;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationScoped
+public class PedidoService {
+
+    private Set<Long> pedidos = new HashSet<>();
+
+    public void newPedido(Long id) {
+        pedidos.add(id);
+    }
+
+    public void cancelPedido(Long id) {
+        pedidos.remove(id);
+    }
+}
